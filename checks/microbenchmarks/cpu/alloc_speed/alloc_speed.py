758bbe6ea70a7c376a96dca9da4a24992c518d3b
jgp <jgp@cscs.ch>
2024-09-08 12:32:46 +0200

    del

--- checks/microbenchmarks/cpu/alloc_speed/alloc_speed.py
+++ checks/microbenchmarks/cpu/alloc_speed/alloc_speed.py
@@ -7,10 +7,23 @@ import reframe as rfm
 import reframe.utility.sanity as sn
 
 
+alloc_reference = {
+    'zen2': {
+        # cce:0.32,0.16 intel:0.13:0.06 gnu:0.14,0.07
+        'no': (0.32, None, 0.15, 's'),
+        '2M': (0.16, None, 0.15, 's'),
+    },
+    'neoverse_v2': {
+        # cce:0.13,0.04 gnu:0.08,0.04
+        'no': (0.13, None, 0.15, 's'),
+        '2M': (0.07, None, 0.15, 's'),
+    },
+}
+
+
 class AllocSpeedTestBase(rfm.RegressionTest):
     sourcepath = 'alloc_speed.cpp'
     build_system = 'SingleSource'
-    # valid_prog_environs = ['*']
 
     @run_after('init')
     def set_descr(self):
@@ -19,33 +32,27 @@ class AllocSpeedTestBase(rfm.RegressionTest):
 
     @run_before('compile')
     def set_cxxflags(self):
+        # avoid -O3 because some compilers over-optimize
         self.build_system.cxxflags = ['-std=c++11']
 
     @sanity_function
     def assert_4GB(self):
         return sn.assert_found('4096 MB', self.stdout)
 
-    @run_before('performance')
-    def set_reference(self):
-        base_perf = 0.2
-        sys_reference = {
-            'no': {
-                '*': {
-                    'time': (base_perf, None, 0.15, 's')
-                }
-            },
-            '2M': {
-                '*': {
-                    'time': (base_perf/2, None, 0.15, 's')
-                }
-            },
-        }
-        self.reference = sys_reference[self.hugepages]
-
     @performance_function('s')
     def elapsed(self):
         return sn.extractsingle(r'4096 MB, allocation time (?P<time>\S+)',
                                 self.stdout, 'time', float)
+
+    @run_before('performance')
+    def set_performance_reference(self):
+        self.skip_if_no_procinfo()
+        proc_info_arch = self.current_partition.processor.info['arch']
+        self.reference = {
+            '*': {
+                'elapsed': alloc_reference[proc_info_arch][self.hugepages]
+            }
+        }
 
 
 @rfm.simple_test
@@ -56,7 +63,7 @@ class CPE_AllocSpeedTest(AllocSpeedTestBase):
     tags = {'production', 'craype'}
 
     @run_after('setup')
-    def skip_builtin_aocc_env(self):
+    def skip_builtin_env(self):
         self.skip_if(self.current_environ.name.startswith('PrgEnv-aocc'))
 
     @run_after('setup')
