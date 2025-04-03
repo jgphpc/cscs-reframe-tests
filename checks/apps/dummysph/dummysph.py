e2f4d31906d5399151433f625fe29edeecef2baa
Jg_Piccinali <>
2025-04-03 07:50:39 +0200

    dummy

--- checks/apps/dummysph/dummysph.py
+++ checks/apps/dummysph/dummysph.py
@@ -18,7 +18,7 @@ import reframe.utility.sanity as sn
 # }
 
 
-# {{{ DummySPH_Uenv_Ascent+-tipsy
+# {{{ DummySPH_Uenv_Ascent
 @rfm.simple_test
 class DummySPH_Uenv_Ascent(rfm.RegressionTest):
     """
@@ -26,17 +26,19 @@ class DummySPH_Uenv_Ascent(rfm.RegressionTest):
     -c cscs-reframe-tests.git/checks/apps/dummysph/dummysph.py \
     --system santis:normal -l
     """
-    valid_systems = ['+uenv']
-    valid_prog_environs = ['+ascent']
-    version = variable(str, value='rfm')
-    strided = parameter(['ON', 'OFF'])
+    valid_systems = ['+remote']
+    valid_prog_environs = ['+uenv +ascent -cuda', '+uenv +ascent +cuda']
+    #
+    version = variable(str, value='main')
+    aos = parameter(['ON', 'OFF'])
     # ON = struct tipsy (AOS) / OFF = std::vector (SOA)
-    tipsy = variable(str, value='OFF')
-    tipsy_file = variable(str, value='hr8799_bol_bd1.017300')
+    # tipsy = variable(str, value='OFF')
+    # tipsy_file = variable(str, value='hr8799_bol_bd1.017300')
     h5part = variable(str, value='OFF')
     datadump = variable(str, value='OFF')
+    fp64 = parameter(['OFF', 'ON']) # OFF=<float>, ON=<double>
     num_gpus = variable(int, value=4)
-    sourcesdir = 'https://github.com/jgphpc/DummySPH.git'
+    sourcesdir = 'https://github.com/jfavre/DummySPH.git'
     build_system = 'CMake'
     build_locally = False
     executable = 'hostname'
@@ -49,58 +51,100 @@ class DummySPH_Uenv_Ascent(rfm.RegressionTest):
         self.prebuild_cmds += [f"git checkout {self.version}", "cd src"]
         # TODO: --depth=1
         self.build_system.config_opts = [
-            # -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx
+            # '-DCMAKE_C_COMPILER=mpicc',
+            # '-DCMAKE_CXX_COMPILER=mpicxx',
             '-DCMAKE_BUILD_TYPE=Debug',
-            f'-DSTRIDED_SCALARS={self.strided}',
-            '-DCAN_LOAD_TIPSY=ON' if self.strided == 'ON' else '-DCAN_LOAD_TIPSY=OFF',
+            # '-DCMAKE_BUILD_TYPE=Release',
+            f'-DSTRIDED_SCALARS={self.aos}',
+            # f'-DCAN_LOAD_TIPSY={self.tipsy}',
             f'-DCAN_LOAD_H5Part={self.h5part}',
             f'-DCAN_DATADUMP={self.datadump}',
+            f'-DSPH_DOUBLE={self.fp64}',
             '-DINSITU=Ascent',
             '-DAscent_DIR=/user-environment/env/default/lib/cmake',
         ]
 
     @run_before('run')
     def set_executable_tests(self):
+        self.job.options = [f'--nodes=1']
+        in_dir = '/capstor/store/cscs/userlab/vampir'
+        out_dir = '/dev/shm/$USER'
+#del         tipsy_flag = f'--tipsy {self.tipsy_file}'
         self.rpt = 'rpt'
+#del         self.prerun_cmds += [
+#del             # --- 0:
+#del             f'(mkdir -p 0 ;cd 0; CUDA_VISIBLE_DEVICES=0 {self.exe} &> {self.rpt} ; cd ..) &',
+#del             # --- 1:
+#del             f'(mkdir -p 1 ;cd 1 ;'
+#del             f'ln -s ../Ascent_yaml/pseudocolor/ascent_actions.yaml ;'
+#del             f'CUDA_VISIBLE_DEVICES=1 {self.exe} &> {self.rpt} ; cd ..) &',
+#del             # --- 2:
+#del             f'(mkdir -p 2 ;cd 2 ;'
+#del             f'ln -s ../Ascent_yaml/hdf5/* . ;'
+#del             f'CUDA_VISIBLE_DEVICES=2 {self.exe} &> {self.rpt} ; cd ..) &',
+#del         ]
+#del         if self.aos == 'ON':
+#del             args = f'--tipsy {self.tipsy_file} &> {self.rpt}'
+#del             self.prerun_cmds += [
+#del                 # --- 3: (-cuda)
+#del                 f'(mkdir -p 3 ;cd 3 ;'
+#del                 f'ln -s ../Ascent_yaml/tipsy_box/ascent_actions.yaml . ;'
+#del                 f'cp /capstor/store/cscs/userlab/vampir/{self.tipsy_file} . ;'
+#del                 f'CUDA_VISIBLE_DEVICES=0 {self.exe} {args} ; cd ..) &',
+#del                 # TODO: jfrog
+#del                 # ---
+#del                 'wait'
+#del             ]
+#del         else:
+#del             self.prerun_cmds += ['wait']
+
+#del        if self.tipsy and self.fp64 == 'OFF':
+#del        # and self.aos == 'ON':
+#del            # TODO: jfrog
+#del            self.prerun_cmds += [
+#del                f'ln -s {in_dir}/{self.tipsy_file} .',
+#del                #
+#del                f'{self.exe} {tipsy_flag} --rendering {out_dir}/image+t'
+#del                f' &> {self.rpt}+rt &',
+#del                f'{self.exe} {tipsy_flag} --thresholding {out_dir}/threshold+t'
+#del                f' &> {self.rpt}+tt &',
+#del                f'{self.exe} {tipsy_flag} --compositing {out_dir}/composite+t'
+#del                f' &> {self.rpt}+ct &',
+#del                f'{self.exe} {tipsy_flag} --histsampling {out_dir}/histsampl+t'
+#del                f' &> {self.rpt}+ht &',
+#del            ]
         self.prerun_cmds += [
-            # --- 0:
-            f'(mkdir -p 0 ;cd 0; CUDA_VISIBLE_DEVICES=0 {self.exe} &> {self.rpt} ; cd ..) &',
-            # --- 1:
+            f'mkdir -p {out_dir}',
+            # separate dirs to avoid overwriting simple_trigger_actions.yaml:
+            f'(mkdir -p 0 ;cd 0 ;'
+            f'CUDA_VISIBLE_DEVICES=0 {self.exe} --rendering {out_dir}/image-t'
+            f' &> {self.rpt} ;cd ..) &',
+            #
             f'(mkdir -p 1 ;cd 1 ;'
-            f'ln -s ../Ascent_yaml/pseudocolor/ascent_actions.yaml ;'
-            f'CUDA_VISIBLE_DEVICES=1 {self.exe} &> {self.rpt} ; cd ..) &',
-            # --- 2:
+            f'CUDA_VISIBLE_DEVICES=1 {self.exe} --thresholding {out_dir}/threshold-t'
+            f' &> {self.rpt} ;cd ..) &',
+            #
             f'(mkdir -p 2 ;cd 2 ;'
-            f'ln -s ../Ascent_yaml/hdf5/* . ;'
-            f'CUDA_VISIBLE_DEVICES=2 {self.exe} &> {self.rpt} ; cd ..) &',
+            f'CUDA_VISIBLE_DEVICES=2 {self.exe} --compositing {out_dir}/composite-t'
+            f' &> {self.rpt} ;cd ..) &',
+            #
+            f'(mkdir -p 3 ;cd 3 ;'
+            f'CUDA_VISIBLE_DEVICES=3 {self.exe} --histsampling {out_dir}/histsampl-t'
+            f' &> {self.rpt} ;cd ..) &',
+            'wait',
         ]
-        if self.strided == 'ON':
-            args = f'--tipsy {self.tipsy_file} &> {self.rpt}'
-            self.prerun_cmds += [
-                # --- 3: (-cuda)
-                f'(mkdir -p 3 ;cd 3 ;'
-                f'ln -s ../Ascent_yaml/tipsy_box/ascent_actions.yaml . ;'
-                f'cp /capstor/store/cscs/userlab/vampir/{self.tipsy_file} . ;'
-                f'CUDA_VISIBLE_DEVICES=0 {self.exe} {args} ; cd ..) &',
-                # TODO: jfrog
-                # ---
-                'wait'
-            ]
-        else:
-            self.prerun_cmds += ['wait']
 
         self.postrun_cmds += [
-            f'file 0/datasets/image.00003.png >> 0/{self.rpt}',
-            f'file 1/datasets/s1_03.png >> 1/{self.rpt}',
-            f'file 2/pl_geometric_clip.cycle_000002.root >> 2/{self.rpt}',
-            f'file 3/datasets/pl_geometric_clip.cycle_000000.root >> 3/{self.rpt}' if self.strided == 'ON' else None,
+            f'mv {out_dir}/* .',
         ]
-        self.job.options = [
-            f'--nodes=1',
-        ]
-        self.extra_resources = {
-            'gres': {'gres': f'gpu:{self.num_gpus}'}
-        }
+
+#del        self.postrun_cmds += [
+#del            f'file 0/datasets/image.00003.png >> 0/{self.rpt}',
+#del            f'file 1/datasets/s1_03.png >> 1/{self.rpt}',
+#del            f'file 2/pl_geometric_clip.cycle_000002.root >> 2/{self.rpt}',
+#del            f'file 3/datasets/pl_geometric_clip.cycle_000000.root >> 3/{self.rpt}' if self.aos == 'ON' else None,
+#del        ]
+        self.extra_resources = {'gres': {'gres': f'gpu:{self.num_gpus}'}}
 
     @sanity_function
     def validate_test(self):
@@ -123,12 +167,12 @@ class DummySPH_Uenv_Ascent(rfm.RegressionTest):
             sn.assert_found(regex, rpt_0, msg=f'sanity failed in {rpt_0}'),
             sn.assert_found(regex, rpt_1, msg=f'sanity failed in {rpt_1}'),
             sn.assert_found(regex, rpt_2, msg=f'sanity failed in {rpt_2}'),
-            sn.assert_found(regex, rpt_3, msg=f'sanity failed in {rpt_3}') if self.strided == 'ON' else None,
+            sn.assert_found(regex, rpt_3, msg=f'sanity failed in {rpt_3}') if self.aos == 'ON' else None,
             #
             sn.assert_found(regex_png, rpt_0, msg=f'sanity failed in {rpt_0} png'),
             sn.assert_found(regex_png, rpt_1, msg=f'sanity failed in {rpt_1} png'),
             sn.assert_found(regex_hdf, rpt_2, msg=f'sanity failed in {rpt_2} hdf'),
-            sn.assert_found(regex_hdf, rpt_3, msg=f'sanity failed in {rpt_3} hdf') if self.strided == 'ON' else None,
+            sn.assert_found(regex_hdf, rpt_3, msg=f'sanity failed in {rpt_3} hdf') if self.aos == 'ON' else None,
         ])
         # num_gpus_res = sn.count(sn.extractall(regex, self.stdout, 1, float))
         # return sn.assert_eq(self.num_gpus, num_gpus_res)
@@ -152,30 +196,34 @@ class DummySPH_Uenv_Ascent(rfm.RegressionTest):
 #}}}
 
 
-# {{{ DummySPH_Uenv_VTKm+tipsy
+# {{{ DummySPH_Uenv_VTKm
 @rfm.simple_test
 class DummySPH_Uenv_VTKm(rfm.RegressionTest):
     """
-    UENV=insitu_ascent/develop-nocuda:1730216486 ./R \
+    UENV=insitu_ascent/develop-nocuda:1730216486 ~/R \
     -c cscs-reframe-tests.git/checks/apps/dummysph/dummysph.py \
-    --system santis:normal -J p=debug -n DummySPH_Uenv_VTKm \
-    -r
+    -n DummySPH_Uenv_VTKm \
+    -r \
+    --system santis:normal -J p=debug
     """
-    valid_systems = ['+uenv']
-    valid_prog_environs = ['+ascent']
-    version = variable(str, value='rfm')
-    strided = parameter(['ON'])
+    valid_systems = ['+remote']
+    #ok: valid_prog_environs = ['+uenv +ascent -cuda']
+    valid_prog_environs = ['+uenv +ascent -cuda', '+uenv +ascent +cuda']
+    #
+    version = variable(str, value='main')
+    aos = parameter(['OFF', 'ON'])
     # ON = struct tipsy (AOS) / OFF = std::vector (SOA)
     tipsy = variable(str, value='ON')
     tipsy_file = variable(str, value='hr8799_bol_bd1.017300')
     h5part = variable(str, value='OFF')
     datadump = variable(str, value='OFF')
-    num_gpus = variable(int, value=4)
-    sourcesdir = 'https://github.com/jgphpc/DummySPH.git'
+    fp64 = parameter(['OFF', 'ON']) # OFF=<float>, ON=<double>
+    # num_gpus = variable(int, value=4)
+    sourcesdir = 'https://github.com/jfavre/DummySPH.git'
     build_system = 'CMake'
     build_locally = False
     executable = 'hostname'
-    exe = '../src/bin/dummysph_vtkm'
+    exe = './src/bin/dummysph_vtkm'
 
     @run_before('compile')
     def set_build_system(self):
@@ -184,59 +232,72 @@ class DummySPH_Uenv_VTKm(rfm.RegressionTest):
         self.prebuild_cmds += [f"git checkout {self.version}", "cd src"]
         # TODO: --depth=1
         self.build_system.config_opts = [
-            # -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx
+            # '-DCMAKE_C_COMPILER=mpicc',
+            # '-DCMAKE_CXX_COMPILER=mpicxx',
             '-DCMAKE_BUILD_TYPE=Debug',
-            f'-DSTRIDED_SCALARS={self.strided}',
+            # '-DCMAKE_BUILD_TYPE=Release',
+            f'-DSTRIDED_SCALARS={self.aos}',
             f'-DCAN_LOAD_TIPSY={self.tipsy}',
             f'-DCAN_LOAD_H5Part={self.h5part}',
             f'-DCAN_DATADUMP={self.datadump}',
+            f'-DSPH_DOUBLE={self.fp64}',
             '-DINSITU=VTK-m',
             '-DVTKm_DIR=`find /user-environment/ -name "vtkm-2.2" |grep -m1 cmake`',
         ]
 
     @run_before('run')
     def set_executable_tests(self):
+        self.job.options = [f'--nodes=1']
+        in_dir = '/capstor/store/cscs/userlab/vampir'
+        out_dir = '/dev/shm/$USER'
+        tipsy_flag = f'--tipsy {self.tipsy_file}'
         self.rpt = 'rpt'
-#         self.prerun_cmds += [
-#             # --- 0:
-#             f'(mkdir -p 0 ;cd 0; CUDA_VISIBLE_DEVICES=0 {self.exe} &> {self.rpt} ; cd ..) &',
-#             # --- 1:
-#             f'(mkdir -p 1 ;cd 1 ;'
-#             f'ln -s ../Ascent_yaml/pseudocolor/ascent_actions.yaml ;'
-#             f'CUDA_VISIBLE_DEVICES=1 {self.exe} &> {self.rpt} ; cd ..) &',
-#             # --- 2:
-#             f'(mkdir -p 2 ;cd 2 ;'
-#             f'ln -s ../Ascent_yaml/hdf5/* . ;'
-#             f'CUDA_VISIBLE_DEVICES=2 {self.exe} &> {self.rpt} ; cd ..) &',
-#         ]
-        if self.strided == 'ON':
-            args = f'--tipsy {self.tipsy_file} &> {self.rpt}'
+        self.prerun_cmds += [f'mkdir -p {out_dir}']
+        if self.tipsy and self.fp64 == 'OFF':
+        # and self.aos == 'ON':
+            # TODO: jfrog
             self.prerun_cmds += [
-                # --- 3: (-cuda)
-                f'(mkdir -p 3 ;cd 3 ;'
-                f'ln -s ../Ascent_yaml/tipsy_box/ascent_actions.yaml . ;'
-                f'cp /capstor/store/cscs/userlab/vampir/{self.tipsy_file} . ;'
-                f'CUDA_VISIBLE_DEVICES=0 {self.exe} {args} ; cd ..) &',
-                # TODO: jfrog
-                # ---
-                'wait'
+                f'ln -s {in_dir}/{self.tipsy_file} .',
+                #
+                f'{self.exe} {tipsy_flag} --rendering {out_dir}/image+t'
+                f' &> {self.rpt}+rt &',
+                f'{self.exe} {tipsy_flag} --thresholding {out_dir}/threshold+t'
+                f' &> {self.rpt}+tt &',
+                f'{self.exe} {tipsy_flag} --compositing {out_dir}/composite+t'
+                f' &> {self.rpt}+ct &',
+                f'{self.exe} {tipsy_flag} --histsampling {out_dir}/histsampl+t'
+                f' &> {self.rpt}+ht &',
             ]
-        else:
-            self.prerun_cmds += ['wait']
+
+        self.prerun_cmds += [
+            f'{self.exe} --rendering {out_dir}/image-t'
+            f' &> {self.rpt}-rt &',
+            f'{self.exe} --thresholding {out_dir}/threshold-t'
+            f' &> {self.rpt}-tt &',
+            f'{self.exe} --compositing {out_dir}/composite-t'
+            f' &> {self.rpt}-ct &',
+            f'{self.exe} --histsampling {out_dir}/histsampl-t'
+            f' &> {self.rpt}-ht &',
+            'wait',
+        ]
 
         self.postrun_cmds += [
-#             f'file 0/datasets/image.00003.png >> 0/{self.rpt}',
-#             f'file 1/datasets/s1_03.png >> 1/{self.rpt}',
-#             f'file 2/pl_geometric_clip.cycle_000002.root >> 2/{self.rpt}',
-            f'file /dev/shm/insitu.0001.png >> 3/{self.rpt}' if self.strided == 'ON' else None,
-            # TODO: /dev/shm
+            f'mv {out_dir}/* .',
         ]
-        self.job.options = [
-            f'--nodes=1',
-        ]
-        self.extra_resources = {
-            'gres': {'gres': f'gpu:{self.num_gpus}'}
-        }
+# #             f'file 0/datasets/image.00003.png >> 0/{self.rpt}',
+# #             f'file 1/datasets/s1_03.png >> 1/{self.rpt}',
+# #             f'file 2/pl_geometric_clip.cycle_000002.root >> 2/{self.rpt}',
+#             f'file /dev/shm/insitu.0001.png >> 3/{self.rpt}' if self.aos == 'ON' else None,
+#             # TODO: clean /dev/shm
+#         ]
+#         self.extra_resources = {
+#             'gres': {'gres': f'gpu:{self.num_gpus}'}
+#         }
+
+#     @run_after('init')
+#     def skip_on_purpose(self):
+#         self.skip_if(self.aos == 'ON' and self.fp64 == 'ON',
+#                      f'Not implemented for AOS and <double>')
 
     @sanity_function
     def validate_test(self):
@@ -259,12 +320,12 @@ class DummySPH_Uenv_VTKm(rfm.RegressionTest):
 #             sn.assert_found(regex, rpt_0, msg=f'sanity failed in {rpt_0}'),
 #             sn.assert_found(regex, rpt_1, msg=f'sanity failed in {rpt_1}'),
 #             sn.assert_found(regex, rpt_2, msg=f'sanity failed in {rpt_2}'),
-            sn.assert_found(regex, rpt_3, msg=f'sanity failed in {rpt_3}') if self.strided == 'ON' else None,
+            sn.assert_found(regex, rpt_3, msg=f'sanity failed in {rpt_3}') if self.aos == 'ON' else None,
             #
 #             sn.assert_found(regex_png, rpt_0, msg=f'sanity failed in {rpt_0} png'),
 #             sn.assert_found(regex_png, rpt_1, msg=f'sanity failed in {rpt_1} png'),
 #             sn.assert_found(regex_hdf, rpt_2, msg=f'sanity failed in {rpt_2} hdf'),
-            sn.assert_found(regex_png, rpt_3, msg=f'sanity failed in {rpt_3} hdf') if self.strided == 'ON' else None,
+            sn.assert_found(regex_png, rpt_3, msg=f'sanity failed in {rpt_3} hdf') if self.aos == 'ON' else None,
         ])
         # num_gpus_res = sn.count(sn.extractall(regex, self.stdout, 1, float))
         # return sn.assert_eq(self.num_gpus, num_gpus_res)
