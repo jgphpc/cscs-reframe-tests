1d829793e4e2717f0749239372f04e84c204499a
Jg Piccinali <jgphpc@users.noreply.github.com>
2026-04-29 10:52:06 +0200

    1

--- config/systems/newton.py
+++ config/systems/newton.py
@@ -4,115 +4,314 @@ import os
 
 
 site_configuration = {
-
-#{{{ systems
-# ./R -c checks/prgenv/mpi_cpi.py -l --system newton:skylake-t32 --prgenv PrgEnv-gnu
+    # {{{ systems
     'systems': [
         {
-            #{{{ skylake
+            # {{{ newton
             'name': 'newton',
-            #'name': 'Newton skylake',
-            'descr': 'https://pmcs2i.ec-lyon.fr/documentation/resources/newton.html',
-            #'hostnames': ['haswell', 'skylake', 'cascade', 'genoa'],
-            #'hostnames': ['prepost-haswell', 'prepost-skylake', 'prepost-cascade', 'prepost-genoa'],
-            'hostnames': ['prepost-skylake'],
+            'descr': 'https://pmcs2i.ec-lyon.fr/documentation/resources/',
+            'hostnames': ['prepost-haswell', 'prepost-skylake',
+                          'prepost-cascade', 'prepost-genoa'],
+            'modules_system': 'tmod4',  # tcl modules
             # max_local_jobs
-            'modules_system': 'tmod4',
-            #'modules'
-            #env_vars
-            #variables
-            #prefix
-            #stagedir
-            #outputdir
+            # modules
+            # env_vars
+            # variables
+            # prefix
+            # stagedir
+            # outputdir
+            # sched_options
             'resourcesdir': '/store/pmcs2i/',
-            #sched_options
             'partitions': [
-#{{{ partition:skylake-t32-[01-14] + 190GB
+                # {{{ haswell-t16-[01-54] - test:haswell-t16-[01-02,30,32]
+                {
+                    'name': 'haswell-t16',
+                    'descr': 'haswell-t16-[01-54] - test nodes * 064G',
+                    'scheduler': 'slurm',
+                    'launcher': 'srun',
+                    'access': ['-p haswell',
+                               '-w haswell-t16-[03-29,31,33-54]'],
+                    'environs': ['builtin', 'PrgEnv-gnu', 'PrgEnv-intel'],
+                    'env_vars': [
+                        ['MODULEPATH',
+                         '/softs/eb/haswell/modules/all:/softs/manual/modules']
+                    ],
+                    'time_limit': '10m',
+                    'max_jobs': 4,
+                    'features': ['cpu', 'remote', 'scontrol'],
+                    'extras': {'cn_memory': 64},
+                },
+                # }}}
+                # {{{ haswell-f20-[01-03]
+                {
+                    'name': 'haswell-f20',
+                    'descr': 'haswell-f20-[01-03] * 270G',
+                    'scheduler': 'slurm',
+                    'launcher': 'srun',
+                    'access': ['-p haswell',
+                               '-w haswell-f20-[01-03]]'],
+                    'environs': ['builtin', 'PrgEnv-gnu', 'PrgEnv-intel'],
+                    'env_vars': [
+                        ['MODULEPATH',
+                         '/softs/eb/haswell/modules/all:/softs/manual/modules']
+                    ],
+                    'time_limit': '10m',
+                    'max_jobs': 4,
+                    'features': ['cpu', 'remote', 'scontrol'],
+                    'extras': {'cn_memory': 270},
+                },
+                # }}}
+                # {{{ haswell-x20-[01-08]
+                {
+                    'name': 'haswell-x20',
+                    'descr': 'haswell-x20-[01-08] * 384G',
+                    'scheduler': 'slurm',
+                    'launcher': 'srun',
+                    'access': ['-p haswell',
+                               '-w haswell-x20-[01-08]]'],
+                    'environs': ['builtin', 'PrgEnv-gnu', 'PrgEnv-intel'],
+                    'env_vars': [
+                        ['MODULEPATH',
+                         '/softs/eb/haswell/modules/all:/softs/manual/modules']
+                    ],
+                    'time_limit': '10m',
+                    'max_jobs': 4,
+                    'features': ['cpu', 'remote', 'scontrol'],
+                    'extras': {'cn_memory': 384},
+                },
+                # }}}
+                # {{{ haswell-x44-01
+                {
+                    'name': 'haswell-x44',
+                    'descr': 'haswell-x44-01 * 512G',
+                    'scheduler': 'slurm',
+                    'launcher': 'srun',
+                    'access': ['-p haswell',
+                               '-w haswell-x44-01'],
+                    'environs': ['builtin', 'PrgEnv-gnu', 'PrgEnv-intel'],
+                    'env_vars': [
+                        ['MODULEPATH',
+                         '/softs/eb/haswell/modules/all:/softs/manual/modules']
+                    ],
+                    'time_limit': '10m',
+                    'max_jobs': 4,
+                    'features': ['cpu', 'remote', 'scontrol'],
+                    'extras': {'cn_memory': 512},
+                },
+                # }}}
+
+                # {{{ cascade-t32-[01-40]
+                {
+                    'name': 'cascade-t32',
+                    'descr': 'cascade-t32-[01-40] * 190G',
+                    'scheduler': 'slurm',
+                    'launcher': 'srun',
+                    'access': ['-p cascade',
+                               '-w cascade-t32-[01-40]'],
+                    'environs': ['builtin', 'PrgEnv-gnu', 'PrgEnv-intel'],
+                    'env_vars': [
+                        ['MODULEPATH',
+                         '/softs/eb/cascade/modules/all:/softs/manual/modules']
+                    ],
+                    'time_limit': '10m',
+                    'max_jobs': 4,
+                    'features': ['cpu', 'remote', 'scontrol'],
+                    'extras': {'cn_memory': 190},
+                },
+                # }}}
+                # {{{ cascade-f32-[01-08]
+                {
+                    'name': 'cascade-f32',
+                    'descr': 'cascade-f32-[01-08] * 384G',
+                    'scheduler': 'slurm',
+                    'launcher': 'srun',
+                    'access': ['-p cascade',
+                               '-w cascade-f32-[01-08]'],
+                    'environs': ['builtin', 'PrgEnv-gnu', 'PrgEnv-intel'],
+                    'env_vars': [
+                        ['MODULEPATH',
+                         '/softs/eb/cascade/modules/all:/softs/manual/modules']
+                    ],
+                    'time_limit': '10m',
+                    'max_jobs': 4,
+                    'features': ['cpu', 'remote', 'scontrol'],
+                    'extras': {'cn_memory': 384},
+                },
+                # }}}
+                # {{{ cascade-x32-[01-04]
+                {
+                    'name': 'cascade-x32',
+                    'descr': 'cascade-x32-[01-04] * 768G',
+                    'scheduler': 'slurm',
+                    'launcher': 'srun',
+                    'access': ['-p cascade',
+                               '-w cascade-x32-[01-04]'],
+                    'environs': ['builtin', 'PrgEnv-gnu', 'PrgEnv-intel'],
+                    'env_vars': [
+                        ['MODULEPATH',
+                         '/softs/eb/cascade/modules/all:/softs/manual/modules']
+                    ],
+                    'time_limit': '10m',
+                    'max_jobs': 4,
+                    'features': ['cpu', 'remote', 'scontrol'],
+                    'extras': {'cn_memory': 768},
+                },
+                # }}}
+
+                # {{{ skylake-t32-[01-14]
                 {
                     'name': 'skylake-t32',
-                    'descr': 'skylake-t32-[01-14]',
+                    'descr': 'skylake-t32-[01-14] * 190G',
                     'scheduler': 'slurm',
-                    #sched_options
                     'launcher': 'srun',
-                    #'access'
+                    'access': ['-p skylake',
+                               '-w skylake-t32-[01-14]'],
                     'environs': ['builtin', 'PrgEnv-gnu', 'PrgEnv-intel'],
-                    #"container_platforms": 
-                    #modules
-                    #env_vars
-                    #variables
+                    'env_vars': [
+                        ['MODULEPATH',
+                         '/softs/eb/skylake/modules/all:/softs/manual/modules']
+                    ],
                     'time_limit': '10m',
                     'max_jobs': 4,
-                    #'prepare_cmds':
-                    #processor
-                    #devices
                     'features': ['cpu', 'remote', 'scontrol'],
-                    #extras
-                    #resources
+                    'extras': {'cn_memory': 190},
                 },
-#}}}
-#{{{ partition:skylake-f32-[01-06] + 384GB
+                # }}}
+                # {{{ skylake-f32-[01-06]
                 {
                     'name': 'skylake-f32',
-                    'descr': 'skylake-f32-[01-06]',
+                    'descr': 'skylake-f32-[01-06] * 384G',
                     'scheduler': 'slurm',
-                    #sched_options
                     'launcher': 'srun',
-                    #'access'
+                    'access': ['-p skylake',
+                               '-w skylake-f32-[01-06]'],
                     'environs': ['builtin', 'PrgEnv-gnu', 'PrgEnv-intel'],
-                    #"container_platforms": 
-                    #modules
-                    #env_vars
-                    #variables
+                    'env_vars': [
+                        ['MODULEPATH',
+                         '/softs/eb/skylake/modules/all:/softs/manual/modules']
+                    ],
                     'time_limit': '10m',
                     'max_jobs': 4,
-                    #'prepare_cmds':
-                    #processor
-                    #devices
-                    #features
-                    #extras
-                    #resources
-                },
-#}}}
-#{{{ partition:login
-                {
-                    'name': 'login',
-                    'descr': 'Login nodes',
-                    'scheduler': 'local',
-                    #sched_options
-                    'launcher': 'local',
-                    #'access'
-                    'environs': ['builtin', 'PrgEnv-gnu', 'PrgEnv-intel'],
-                    #"container_platforms": 
-                    #modules
-                    #env_vars
-                    #variables
-                    'time_limit': '10m',
-                    'max_jobs': 4,
-                    #'prepare_cmds':
-                    #processor
-                    #devices
                     'features': ['cpu', 'remote', 'scontrol'],
-                    #extras
-                    #resources
-                }
-#}}}
+                    'extras': {'cn_memory': 384},
+                },
+                # }}}
+
+                # {{{ genoa-t64-[01-08]
+                {
+                    'name': 'genoa-t64',
+                    'descr': 'genoa-t64-[01-08] * 384G',
+                    'scheduler': 'slurm',
+                    'launcher': 'srun',
+                    'access': ['-p genoa',
+                               '-w genoa-t64-[01-08]'],
+                    'environs': ['builtin', 'PrgEnv-gnu', 'PrgEnv-intel'],
+                    'env_vars': [
+                        ['MODULEPATH',
+                         '/softs/eb/genoa/modules/all:/softs/manual/modules']
+                    ],
+                    'time_limit': '10m',
+                    'max_jobs': 4,
+                    'features': ['cpu', 'remote', 'scontrol'],
+                    'extras': {'cn_memory': 384},
+                },
+                # }}}
+
+                # {{{ prepost-haswell
+                {
+                    'name': 'prepost-haswell',
+                    'descr': 'Haswell Login nodes',
+                    'scheduler': 'local',
+                    'launcher': 'local',
+                    'environs': ['builtin', 'PrgEnv-gnu', 'PrgEnv-intel'],
+                    'env_vars': [
+                        ['MODULEPATH',
+                         '/softs/eb/haswell/modules/all:/softs/manual/modules']
+                    ],
+                    'time_limit': '10m',
+                    'max_jobs': 4,
+                    'features': ['cpu', 'scontrol'],
+                    # variables
+                    # prepare_cmds
+                    # processor
+                    # devices
+                    # extras
+                    # resources
+                },
+                # }}}
+                # {{{ prepost-cascade
+                {
+                    'name': 'prepost-cascade',
+                    'descr': 'Cascade Login nodes',
+                    'scheduler': 'local',
+                    'launcher': 'local',
+                    'environs': ['builtin', 'PrgEnv-gnu', 'PrgEnv-intel'],
+                    'env_vars': [
+                        ['MODULEPATH',
+                         '/softs/eb/cascade/modules/all:/softs/manual/modules']
+                    ],
+                    'time_limit': '10m',
+                    'max_jobs': 4,
+                    'features': ['cpu', 'scontrol'],
+                },
+                # }}}
+                # {{{ prepost-skylake
+                {
+                    'name': 'prepost-skylake',
+                    'descr': 'Skylake Login nodes',
+                    'scheduler': 'local',
+                    'launcher': 'local',
+                    'environs': ['builtin', 'PrgEnv-gnu', 'PrgEnv-intel'],
+                    'env_vars': [
+                        ['MODULEPATH',
+                         '/softs/eb/skylake/modules/all:/softs/manual/modules']
+                    ],
+                    'time_limit': '10m',
+                    'max_jobs': 4,
+                    'features': ['cpu', 'scontrol'],
+                },
+                # }}}
+                # {{{ prepost-genoa
+                {
+                    'name': 'prepost-genoa',
+                    'descr': 'Genoa Login nodes',
+                    'scheduler': 'local',
+                    'launcher': 'local',
+                    'environs': ['builtin', 'PrgEnv-gnu', 'PrgEnv-intel'],
+                    'env_vars': [
+                        ['MODULEPATH',
+                         '/softs/eb/genoa/modules/all:/softs/manual/modules']
+                    ],
+                    'time_limit': '10m',
+                    'max_jobs': 4,
+                    'features': ['cpu', 'scontrol'],
+                },
+                # }}}
+
             ]  # partitions
-            #}}}
+            # }}}
         }  # systems
     ],     # systems
-#}}}
-#{{{ environments
+    # }}}
+    # {{{ environments
     'environments': [
+        {
+            'name': 'builtin',
+            'cc': 'gcc',
+            'cxx': 'g++',
+            'ftn': 'gfortran',
+            'features': ['serial', 'openmp', 'alloc_speed', 'hdf5'],
+        },
         {
             'name': 'PrgEnv-gnu',
             'cc': 'mpicc',
             'cxx': 'mpicxx',
             'ftn': 'mpif90',
-            #'target_systems': [''],
+            # 'target_systems': [''],
             'features': [
                 'serial', 'openmp', 'mpi', 'alloc_speed', 'hdf5'
             ],
-            'modules': ['foss/2025b']
+            'modules': ['foss']  # hwl:foss/2023a, else:foss/2025b
         },
         {
             'name': 'PrgEnv-intel',
@@ -122,9 +321,9 @@ site_configuration = {
             'features': [
                 'serial', 'openmp', 'mpi', 'alloc_speed', 'hdf5'
             ],
-            'modules': ['intel/2025a']
+            'modules': ['intel']  # hwl:intel/2023a, else:intel/2025a
         }
     ]
-#}}} environments
+    # }}} environments
 
 }  # site_configuration
