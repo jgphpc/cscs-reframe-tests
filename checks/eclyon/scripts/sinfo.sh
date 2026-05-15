4f8de740a95927c673ce040c1c96d4e23cea6c24
Jg Piccinali <jgphpc@users.noreply.github.com>
2026-05-15 13:17:24 +0200

    sinfo

---
+++ checks/eclyon/scripts/sinfo.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+partition=$1
+
+scontrol show partition $partition &> /dev/null
+if [ $? -ne 0 ] ;then
+    echo "# partition=$partition not found"
+    scontrol show partition |grep PartitionName=
+    return
+fi
+
+# partition state:
+sinfo -s |egrep "TIMELIMIT|^$partition"
+
+# report nodes state in partition:
+nodelist_a=$(scontrol show partition $partition |grep " Nodes=" |cut -d= -f2)
+
+# --- list of nodes name:
+hostlist -e $nodelist_a > .eff.1
+# --- list of nodes state:
+scontrol show nodes $nodelist_a |grep "State=" |awk '{print $1}' |cut -d= -f2 |awk '{print "@"$0"@"}' > .eff.2
+list_of_states=$(sort -u .eff.2)
+# list_of_states=$(scontrol show nodes $nodelist_a |grep "State=" |awk '{print $1}' |cut -d= -f2 |sort -u)
+paste -d " " .eff.1 .eff.2 > .eff.3
+
+for ss in $list_of_states ;do
+    nodelist=`hostlist -c $(grep "$ss" .eff.3 |awk '{print $1}')`
+    echo "# $ss $nodelist" |tr -d @
+done
+
+rm -f .eff.1 .eff.2 .eff.3
+
