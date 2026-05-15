c7e06b65cee74c0f51ed1f93ab4f2422bf25950d
Jg Piccinali <jgphpc@users.noreply.github.com>
2026-05-15 11:38:19 +0200

    1

--- checks/eclyon/scripts/distribute_partition+idle.sh
+++ checks/eclyon/scripts/distribute_partition+idle.sh
@@ -1,39 +1,35 @@
 #!/bin/bash
 
 # https://confluence.cscs.ch/spaces/reframe/pages/917505368/Flexible+Tests
+# source checks/eclyon/scripts/distribute_partition+idle.sh haswell && echo nodelist=$nodelist
 
 partition=$1
 
 rc=$(scontrol show nodes --json &> /dev/null; echo $?)    
 if [ $rc ] ;then
-    json='Y'
-else
+    json='N'
     # --json not supported
     # scontrol: fatal: serializer_required: could not find plugin for application/json
-    json='N'
+else
+    json='Y'
 fi
-
-# exit 0
 
 # get list of IDLE compute nodes from 1 partition
 if [ "$json" = "Y" ] ;then
     nodelist_=$(scontrol show partition --json $partition |jq -r .partitions[0].nodes.configured)
-
-    hostlist -e $nodelist_ > .eff.1
-    scontrol show nodes $nodelist_ |grep "State=" |awk '{print $1}' |cut -d= -f2 > .eff.2
-    nodelist=$(paste .eff.1 .eff.2 |grep IDLE |awk '{printf "%s,",$1}' |xargs hostlist -c)
-    rm -f .eff.1 .eff.2
-
-    nnodes=$(hostlist -e $nodelist |wc -l)
+    nodelist=$(scontrol show nodes --json $nodelist_ |jq -rc '.nodes[] | select(.state | index("IDLE")) | .hostname' |xargs hostlist -c)
 fi
 
 if [ "$json" = "N" ] ;then
     nodelist_=$(scontrol show partition $partition |grep " Nodes=" |cut -d= -f2)
-
+    hostlist -e $nodelist_ > .eff.1
+    scontrol show nodes $nodelist_ |grep "State=" |awk '{print $1}' |cut -d= -f2 > .eff.2
+    nodelist=$(paste -d " " .eff.1 .eff.2 |grep " IDLE$" |awk '{printf "%s,",$1}' |xargs hostlist -c)
+    rm -f .eff.1 .eff.2
 fi
 
+nnodes=$(hostlist -e $nodelist |wc -l)
 echo "nodelist=$nodelist # $nnodes"
+export nodelist=$nodelist
 
 # ./R -c checks/system/slurm/slurm.py -n SlurmParanoidCheck -r --distribute -J w=$nodelist
-
-exit 0
