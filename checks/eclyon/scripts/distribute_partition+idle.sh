5444ad79d633dd607fa505594006a3a3fbf658a4
Jg Piccinali <jgphpc@users.noreply.github.com>
2026-05-15 11:55:55 +0200

    1

--- checks/eclyon/scripts/distribute_partition+idle.sh
+++ checks/eclyon/scripts/distribute_partition+idle.sh
@@ -3,12 +3,18 @@
 # https://confluence.cscs.ch/spaces/reframe/pages/917505368/Flexible+Tests
 # source checks/eclyon/scripts/distribute_partition+idle.sh haswell && echo nodelist=$nodelist
 
+# sinfo -sa
+#   PARTITION AVAIL  TIMELIMIT   NODES(A/I/O/T) NODELIST
+#   haswell*     up 14-00:00:0      17/28/17/62 haswell-f20-[01-03],haswell-t16-[03-29,31,33-54],haswell-x20-[01-08],haswell-x44-01
+#   skylake      up 7-00:00:00        19/1/0/20 skylake-f32-[01-06],skylake-t32-[01-14]
+#   cascade      up 7-00:00:00       31/21/0/52 cascade-f32-[01-08],cascade-t32-[01-40],cascade-x32-[01-04]
+#   genoa        up 7-00:00:00          8/0/0/8 genoa-t64-[01-08]
+
 # for ii in haswell skylake cascade genoa ;do ./distribute_partition+idle.sh $ii ;done
 #  partition=haswell nodelist=haswell-f20-[02-03],haswell-t16-[19,27-29,35-42,44,48,50,52-53],haswell-x20-[01-08],haswell-x44-01 # 28
 #  partition=skylake nodelist=skylake-f32-06 # 1
 #  partition=cascade nodelist=cascade-f32-[02-04,06,08],cascade-t32-[04,12-13,15-17,22,28,32,35,39-40],cascade-x32-[01-04] # 21
 #  partition=genoa no.idle.nodes.found
-
 
 partition=$1
 
